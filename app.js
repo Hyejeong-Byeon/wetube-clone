cac421a37046c6735cb3934eeefbcf1f41bbb76a
Hyejeong-Byeon <hyejeong.bhj@gmail.com>
2020-06-19 00:50:21 +0900

    Test
    Express Core: Routing

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import {
+    userRouter
+} from "./router.js";
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-    console.log(`Listening on:http://localhost:${PORT}`);
 
 const handleHome = (req, res) =>
     res.send("Hello from home");
@@ -18,8 +16,10 @@ const handleProfile = (req, res) =>
     res.send("You are my profile");
 
 app.use(cookieParser());
-app.use(bodyParse.json());
-app.use(bodyParse.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(helmet());
 app.use(morgan("tiny"));
 
@@ -27,4 +27,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
