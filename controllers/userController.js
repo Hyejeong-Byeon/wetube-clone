6a09acb18f264d76030c7fe4b62439d5e19e446b
Hyejeong-Byeon <hyejeong.bhj@gmail.com>
2020-06-25 03:08:35 +0900

    Log In and User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ export const getJoin = (req, res) => {
     res.render("join", {
         pageTitle: "Join"
     });
-}
+};
 
 export const postJoin = (req, res) => {
     const {
@@ -28,9 +28,16 @@ export const postJoin = (req, res) => {
 
 };
 
-export const login = (req, res) => res.render("login", {
-    pageTitle: "Login"
-});
+export const getLogin = (req, res) => {
+    res.render("login", {
+        pageTitle: "Login"
+    });
+};
+
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+
 export const logout = (req, res) => res.render("logout", {
     pageTitle: "Logout"
 });
