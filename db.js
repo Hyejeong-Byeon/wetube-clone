ead0e037954b8b2e585b51af0734b578b0ddc406
Hyejeong-Byeon <hyejeong.bhj@gmail.com>
2020-06-30 01:01:12 +0900

    Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,50 +1,14 @@
-export const videos = [
-    {
-        id: 324393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "hyejeong",
-            email: "hj@mail.com"
-        }
-    },
-    {
-        id: 1234567,
-        title: "Video super",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "hyejeong",
-            email: "hj@mail.com"
-        }
-    },
-    {
-        id: 5555555,
-        title: "Video nice",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "hyejeong",
-            email: "hj@mail.com"
-        }
-    },
-    {
-        id: 111111,
-        title: "Video perfect",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "hyejeong",
-            email: "hj@mail.com"
-        }
-    }
-]
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = error => console.log("❌ Error on DB Connection:${error}");
+
+db.once("open", handleOpen);
+db.on("error", handleError);
